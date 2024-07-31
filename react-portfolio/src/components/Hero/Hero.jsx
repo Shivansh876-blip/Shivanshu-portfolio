05cf1e1929d8985bf1a69149c5e7c3c211640465
SudhanshuGupta2206 <guptasudhanshu548@gmail.com>
2024-08-01 00:20:56 +0530

    f

--- react-portfolio/src/components/Hero/Hero.jsx
+++ react-portfolio/src/components/Hero/Hero.jsx
@@ -12,7 +12,7 @@ export const Hero = () => {
           I'm a Frontend Developer using Html,Css,JavaScript,React.js and programming language
           Java and for Database Sql.
         </p>
-        <a href="./assets/REsume/Shivanshu Gupta Resume.pdf download" download className={styles.contactBtn}>
+        <a href="./assets/REsume/Shivanshu Gupta Resume.pdf" download className={styles.contactBtn}>
           Download
         </a>
       </div>
